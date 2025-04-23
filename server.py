2d2f93cf2359fd7ccc78e94cc9df20d6b1cd8dc9
smenegar <smenegar@mail.tigo.com.co>
2025-04-22 22:14:55 -0500

    se crea servidor

---
+++ server.py
@@ -0,0 +1,19 @@
+from mcp.server.fastmcp import FastMCP
+import requests
+app = FastMCP("dorito")
+
+
+@app.tool()
+def obtener_habilidades_pokemon(nombre_pokemon:str) -> list:
+    url         = f"https://pokeapi.co/api/v2/pokemon/{nombre_pokemon}"
+    response    = requests.get(url)
+
+    if response.status_code == 200:
+        pokemon = response.json()
+        habilidades = [habilidad["ability"]["name"] for habilidad in pokemon["abilities"]]
+        return habilidades
+    else:
+        return None
+    
+if __name__ == "__main__":
+    app.run(transport="sse",port="9000")
