85805bb7f0f9240fd972f6559ea6b540e6d61659
smenegar <smenegar@mail.tigo.com.co>
2025-04-22 22:50:18 -0500

    se crea codigo de cliente

---
+++ client.py
@@ -0,0 +1,18 @@
+from mcp import ClientSession
+from mcp.client.sse import sse_client
+import asyncio
+
+# Función principal
+async def run():
+    url_servidor = "http://192.168.1.7:8000/sse"
+    async with sse_client(url=url_servidor,timeout=5.0,sse_read_timeout=300.0,) as (read, write):
+        async with ClientSession( read, write) as session:
+            await session.initialize()
+            tools = await session.list_tools()
+            print("Herramientas disponibles:")
+            for tool in tools.tools:
+                print(f"- {tool.name}: {tool.description}")
+
+# Ejecutar
+if __name__ == "__main__":
+    asyncio.run(run())
